969a753dc9af4100fd1bf5927f7cbb4a614ae3bc
JorgeDLLC <jorge.llanos.caceres@gmail.com>
2025-02-21 11:25:06 -0500

    asdasd

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-<title>Mi Proyecto en React</title>;
+
 createRoot(document.getElementById("root")!).render(
   <HashRouter>
     <App />
