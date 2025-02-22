3e0fee482435b27a077cdb14a7d1f53ebce19488
JorgeDLLC <jorge.llanos.caceres@gmail.com>
2025-02-22 14:38:39 -0500

    Google adsense en apptsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Navbar from "./components/navbar";
@@ -5,11 +6,24 @@ import Basiconas from "./sections/Basiconas";
 import Atrevidas from "./sections/Atrevidas";
 import ContraMujeres from "./sections/ContrapuntoMujeres";
 import ContraHombres from "./sections/ContrapuntoHombres";
-
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2324681488363907";
+    script.async = true;
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script); // Limpia el script al desmontar el componente
+    };
+  }, []);
+
   document.title = "Coplas Carnaval";
+
   return (
     <div className="App">
       <div className="Navbar">
